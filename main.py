5519568dddafdf7ae9457508a8f679765ef69c8a
Omar A Rodriguez <omar@industrialtoys.com>
2016-10-27 21:07:46 -0700

    Create GAE app using flask

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
+import logging
 import requests
+from flask import Flask
+
+# NOTE(omar): required in order to use requests lib in Google App Engine
+# Use the App Engine Requests adapter. This makes sure that Requests uses URLFetch.
+from requests_toolbelt.adapters import appengine
+appengine.monkeypatch()
+
+# init flask app
+app = Flask( __name__ )
 
 #
-# URL for getting the scoreboard info
+# NOTE(omar): URL for getting the scoreboard info
 # http://games.espn.com/ffl/api/v2/scoreboard2?leagueId=212084&matchupPeriodId=4&includeTopScorer=true&rand=44868
 #
 
-LEAGUE_ID = 212084
-WEEK_ID = 4
-
 def get_espn_ff_scores( in_league_id, in_week_id ):
-	scoreboard_url = 'http://games.espn.com/ffl/api/v2/scoreboard2?leagueId={}&matchupPeriodId={}&includeTopScorer=true&rand=44868'.format( in_league_id, in_week_id )
+	scoreboard_url = 'http://games.espn.com/ffl/api/v2/scoreboard2?leagueId={}&matchupPeriodId={}&includeTopScorer=false&rand=44868'.format( in_league_id, in_week_id )
 	r = requests.post( scoreboard_url )
 
 	json_raw = r.json()
 
+	scores = ''
+
 	for matchup in json_raw[ 'scoreboard'][ 'matchups' ]:
 		for T in matchup[ 'teams' ]:
-			print( '{} {} scored {}'.format( T[ 'team' ][ 'teamLocation' ], T[ 'team' ][ 'teamNickname' ], T[ 'score' ] ) )
-		print( '\n' )
+			scores += '{} {} scored {}\n'.format( T[ 'team' ][ 'teamLocation' ], T[ 'team' ][ 'teamNickname' ], T[ 'score' ] )
+		scores += '\n'
+
+	return '<pre>{}</pre>'.format( scores )
+
+#
+# endpoints
+#
+@app.route( '/' ) #, methods=[ 'POST' ] )
+def index():
+	# league_id = request.form[ 'league_id' ]
+	# week_id = request.form[ 'week_id' ]
+	return get_espn_ff_scores( 212084, 4 )
+
+@app.errorhandler( 500 )
+def server_error( e ):
+	logging.exception( 'Error occurred during a request' )
+	return 'Error occurred', 500
 
 
+#
+# test
+#
 if __name__ == '__main__':
-	get_espn_ff_scores( LEAGUE_ID, WEEK_ID )
+	get_espn_ff_scores( 212084, 4 )
 
